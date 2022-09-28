007a712bffa1fa6059b199b107f5eda5ff48d058
[carlos martinez] <theycallhellp@gmail.com>
2022-09-28 14:35:16 -0700

    update

--- course-content/03-javascript/activities/14-Stu_Iteration/Unsolved/script.js
+++ course-content/03-javascript/activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,10 @@
 //WRITE YOUR CODE BELOW
+var students = ["Angel" , "Carlos", "Peter", "Kevin", "Jerome"];
+
+for (var i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
+
+for (var i = 0; i < students.length; i++) {
+    console.log("Gret to see you, " + students[i]);
+}
--- course-content/03-javascript/activities/22-Stu_Objects/Unsolved/script.js
+++ course-content/03-javascript/activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,22 @@
 //WRITE YOUR CODE BELOW
+var customerOrder = {
+    drinkName: "Coffee",
+    numOfSugars: 3,
+    isOrderReady: false
+}
+
+console.log("Drink Name: " + customerOrder.drinkName + ". Number of sugars: " + customerOrder.numOfSugars);
+
+function isReady(order) {
+    if(order.isOrderReady) {
+        console.log("Ready for pick-up");
+    } else {
+        console.log("Still in order queue");
+    }
+}
+
+isReady(customerOrder);
+
+customerOrder.isOrderReady = true;
+
+isReady(customerOrder);
