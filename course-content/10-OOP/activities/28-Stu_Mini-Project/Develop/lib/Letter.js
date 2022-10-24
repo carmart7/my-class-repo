7e5a2aeb961012eeb72781ddb123dbb15a58cf0d
[carlos martinez] <theycallhellp@gmail.com>
2022-10-24 13:16:40 -0700

    update

--- course-content/10-OOP/activities/28-Stu_Mini-Project/Develop/lib/Letter.js
+++ course-content/10-OOP/activities/28-Stu_Mini-Project/Develop/lib/Letter.js
@@ -1,5 +1,35 @@
 class Letter {
-  
+    constructor(letter) {
+        this.char = letter;
+        if(isNaN(letter) && (letter.toUpperCase() == letter.toLowerCase())) {
+            this.visible = true;
+        } else if (letter == ' ') {
+            this.visible = true;
+        } else {
+            this.visible = false;
+        }
+    }
+
+    toString() {
+        if(this.visible) {
+            return this.char;
+        } else {
+            return '_';
+        }
+    }
+
+    guess(guessLetter) {
+        if(guessLetter.toLowerCase() == this.char.toLowerCase()) {
+            this.visible = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getSolution() {
+        return this.char;
+    }
 }
 
 module.exports = Letter;
--- course-content/10-OOP/activities/28-Stu_Mini-Project/Develop/lib/Word.js
+++ course-content/10-OOP/activities/28-Stu_Mini-Project/Develop/lib/Word.js
@@ -1,7 +1,47 @@
 const Letter = require("./Letter");
 
-class Word {
-  
+class Word{
+    constructor(word) {
+        this.letters = [];
+        for(const letter of word) {
+            this.letters.push(new Letter(letter));
+        }
+    }
+
+    guessLetter(charGuess) {
+        let found = false;
+        for(const letter of this.letters) {
+            if(letter.guess(charGuess)) {
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    guessedCorrectly() {
+        for(const letter of this.letters) {
+            if (!letter.visible) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    toString() {
+        let string = '';
+        for(const letter of this.letters) {
+            string += letter.toString();
+        }
+        return string;
+    }
+
+    getSolution() {
+        let string = '';
+        for(const letter of this.letters) {
+            string += letter.getSolution();
+        }
+        return string;
+    }
 }
 
 module.exports = Word;
