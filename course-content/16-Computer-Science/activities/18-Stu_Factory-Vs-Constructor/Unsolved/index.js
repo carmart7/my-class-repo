31515e77f35152001b1340074e1f466c43cbc0f3
[carlos martinez] <theycallhellp@gmail.com>
2022-11-18 11:02:23 -0800

    update

--- course-content/16-Computer-Science/activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
+++ course-content/16-Computer-Science/activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
@@ -1,13 +1,32 @@
-// TODO: Refactor the class to a factory function.
-class Lesson {
-  constructor() {
-    this.title = 'Unit 17 - Computer Science';
-    this.description = 'CS for JS';
-  }
-  information() {
-    console.log(this.title, this.description);
-  }
+// // TODO: Refactor the class to a factory function.
+// class Lesson {
+//   constructor() {
+//     this.title = 'Unit 17 - Computer Science';
+//     this.description = 'CS for JS';
+//   }
+//   information() {
+//     console.log(this.title, this.description);
+//   }
+// }
+
+// const csForJS = new Lesson();
+// csForJS.information();
+
+const logInformation = (state) => ({
+  information: () => console.log(state.title, state.description),
+});
+
+const Lesson = (title, description) => {
+  const state = {
+    title,
+    description
+  };
+
+  return {
+    ...logInformation(state),
+  };
+
 }
 
-const csForJS = new Lesson();
+const csForJS = Lesson('Unit 17 - Computer Science', 'CS for JS');
 csForJS.information();
